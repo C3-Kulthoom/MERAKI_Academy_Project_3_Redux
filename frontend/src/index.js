dcdd9046a28f8b81b059b6d06aa1b89d1df4cc1d
G11_Kulthoom <kulthumradi94@gmail.com>
2021-09-30 22:49:50 +0300

    provider DONE

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-
+import { Provider } from "react-redux";
+// import store
+import store from "./reducer";
 //context providers
-import AuthProvider from "./context/auth";
+
 
 ReactDOM.render(
   <Router>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+
+<Provider store={store}>
+    <App />
+  </Provider>,
+  
   </Router>,
   document.getElementById("root")
 );
